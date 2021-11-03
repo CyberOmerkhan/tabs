b58338002dbd9aa42e1a32d28502c52ed89d847f
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-03 16:07:56 +0600

    beta version

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-let btn1 = document.getElementById('button1');
-let btn2 = document.querySelector('#button2');
-let btn3 = document.getElementsByTagName('button')[2];
-let tab1 = document.querySelector('.tabs_item1');
-let tab2 = document.querySelector('.tabs_item2');
-let tab3 = document.querySelector('.tabs_item3');
-btn1.addEventListener('click', function(){
-    btn1.classList.add('active');
-    btn2.classList.remove('active');
-    btn3.classList.remove('active');
-    tab1.classList.add('acive');
-    tab2.classList.remove('active');
-    tab3.classList.remove('active');
-});
-btn2.addEventListener('click', function(){
-    btn1.classList.remove('active');
-    btn2.classList.add('active');
-    btn3.classList.remove('active');
-    tab1.classList.remove('acive');
-    tab2.classList.add('active');
-    tab3.classList.remove('active');
-});
-btn3.addEventListener('click', function(){
-    btn1.classList.remove('active');
-    btn2.classList.remove('active');
-    btn3.classList.add('active');
-    tab1.classList.remove('acive');
-    tab2.classList.remove('active');
-    tab3.classList.add('active');
+const tabsBtn= document.querySelectorAll('.tabs_nav-btn');
+const tabsItems = document.querySelectorAll('.tabs_item');
+tabsBtn.forEach(function(item){
+    item.addEventListener('click', function(){
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentItem = document.querySelector(tabId);
+        if(!currentItem.classList.contains('active')){
+            tabsBtn.forEach(function(item){
+                item.classList.remove('active');
+            });
+            tabsItems.forEach(function(item){
+                item.classList.remove('active');
+            });
+            currentBtn.classList.add('active');
+            currentItem.classList.add('active');
+        }
+    })
 })
